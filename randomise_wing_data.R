3594a4065a1199e4963dbb523f961019c3a8278a
Cara Conradsen <cara.conradsen@uqconnect.edu.au>
2024-03-15 18:29:23 +0000

    Completed the create_randomised_wing_data and wrote code to implement in parallel

--- randomise_wing_data.R
+++ randomise_wing_data.R
@@ -7,13 +7,17 @@
 ### the 12 groups.
 
 # [1] "Animal"    "Gen"       "Treatment" "Treat"     "Line"      "Vial"      "Ind"       "MD"       
-# [9] "multiout"  "Index"     "Trait"     "Score"     "stdScore"  "outSD" 
+# [9] "multiout"  "Index"     "Trait"     "Score" 
 
 # Packages
-library(data.table); library(purrr); library(parallel); library(dplyr)
+library(data.table); library(foreach); library(parallel); library(doParallel)
 
 # create an output directory
-dir.create("generated_datasets")
+outdir <- "generated_datasets"
+
+if (file.exists(outdir)==FALSE){
+  dir.create(outdir)
+}
 
 # Import wing data
 wing_dat <- fread("mr_wings_6traits.csv")
@@ -40,7 +44,7 @@ population_info <- unique(wing_data_wide[, c("Gen", "Treatment", "Line")])
 nrow(population_info) # 493 unique 'lines'
 
 # Because line 42 in gen 1 S is different from line 42 in gen 2 B, 
-# need to create a unique line identifier
+# need to create a unique line identifier which we will use to randomise lines
 population_info$LineID = 1:nrow(population_info)
 
 # Add the unique line identifier back to the observed data, 
@@ -53,16 +57,72 @@ length(unique(wing_data_wide$LineID)) # should be 493
 
   
 # Line and Trait Information
-line_trait_info <- wing_data_wide[,!c("Gen","Treatment","Treat", "Line")]
+line_trait_info <- wing_data_wide[,!c("Gen","Treatment","Treat", "Line")]# 
 
 # Remove LineID from population_info (this will be ran)
 population_info <- population_info[, .SD, .SDcols = !"LineID"]
 
-### Part 2. Randomise datasets ####
+### Part 2. Write a function to randomised data and save to .csv ####
 
-# start_time <- Sys.time()
+# pop_dat=population_info; biol_dat=line_trait_info; line_smpl=all_sets[,1]
+
+
+create_randomise_wing_data <- function(line_smpl,
+                                pop_dat=population_info, biol_dat=line_trait_info){
+  # set default pop_dat as population_info and  biol_dat as line_trait_info
+  rep = names(line_smpl)
+  pop_dat$LineID = line_smpl
+  randomised_line_data <- biol_dat[pop_dat, on = "LineID"]
+  randomised_line_data$LineID <- NULL # remove LineID
+  
+  # Add Mahalanobis Distance and outliers
+  x <- randomised_line_data[, .SD, .SDcols = c("CS","ILD1.2","ILD1.5","ILD2.5","ILD2.8","ILD3.7")]
+  randomised_line_data$MD <- mahalanobis(x, colMeans(x), cov(x))
+  chi_crit_val <- qchisq(0.999, df=6)
+  randomised_line_data$multiout <- randomised_line_data[,fcase(MD >= chi_crit_val, 1,
+                                                               MD < chi_crit_val, 0, 
+                                                               default = NA)]
+  
+  # Add treatment as an integer "S" = 1 and "B" = 3
+  randomised_line_data$Treat <- randomised_line_data[,fcase(Treatment=="S", 1,
+                                                            Treatment=="B", 3, 
+                                                               default = NA)]
+  
+  # Convert to long
+  randomised_line_data_lng <- melt(randomised_line_data, 
+                                   id.vars=c("Gen","Treatment","Treat","Line", 
+                                             "Animal", "Vial", "Ind", "MD", "multiout"),
+                                   variable.name = "Index", value.name = "Score")
+  
+  # Add integer code for traits
+  randomised_line_data_lng <- trait_number[randomised_line_data_lng, on = "Index"]
+  
+
+  # re-order columns
+  setcolorder(randomised_line_data_lng,
+           c("Animal", "Gen","Treatment", "Treat", "Line",  "Vial","Ind", "MD", 
+             "multiout", "Index", "Trait", "Score"))
+  
+  # sort
+  setorderv(randomised_line_data_lng,
+            c("Gen", "Treat", "Line","Vial", "Ind"))
+  
+  # Output csv
+  fwrite(randomised_line_data_lng, 
+         file=paste0(outdir, "/mr_wings_6traits_rando_", rep, ".csv"),
+         quote=FALSE)
+
+}
+
+
+### Implement function on sets of randomised lines ####
+
+# Create data frame of randomised sets of 'lines'
+
 # Set number of data sets to generate
-n_set = 5
+n_set = 20
+# sample with replacement?
+smpl_with_replace = TRUE
 
 # Create a data.frame of random line samples,
 # where each column is a unique draw of line ids 1:493
@@ -71,26 +131,18 @@ num_lines = length(line_id_list)
 
 all_sets <-data.table(NULL)
 for (i in 1:n_set) {
-  sub_set <- sample(line_id_list, size = num_lines, replace = FALSE)
+  sub_set <- sample(line_id_list, size = num_lines, replace = smpl_with_replace)
   all_sets <- cbind(all_sets,sub_set)
 }
 colnames(all_sets) <- as.character(seq(1,n_set))
-# end_time <- Sys.time()
-# end_time - start_time
-# 1000 number of sets takes ~7.073825 secs
 
-# Determine number of uniquely sampled 'lines'
-#length(unique(unlist(c(all_sets[,1]))))
+# Detect and set cores for parallel processing
+n_cores <- detectCores()-1
+cluster <- makeCluster(n_cores)
+doParallel::registerDoParallel(cluster)
 
-create_randomise_wing_data <- function(line_smpl,
-                                pop_dat=population_info, biol_dat=line_trait_info){
-  # set default pop_dat as population_info and  biol_dat as line_trait_info
-  rep = names(line_smpl)
-  pop_dat$LineID = line_smpl
-  merge(pop_dat, biol_dat, by="LineID", all.y = TRUE)
-}
+# Implement function in parallel
+foreach(x = 1:ncol(all_sets), .packages = c("data.table")) %dopar%
+  create_randomise_wing_data(all_sets[,..x])
 
 
-
-# Check for multivariate outliers
-# MD multiout

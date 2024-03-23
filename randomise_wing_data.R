565c60987e9d6e0b53b08f3b3a964bdf4996db74
Cara Conradsen <uqcconra@uq.edu.au>
2024-03-24 04:06:42 +1000

    Corrected file name bracket direction

--- randomise_wing_data.R
+++ randomise_wing_data.R
@@ -156,11 +156,11 @@ for (i in 1:8) {
   
   # Output csv outdir, 
   fwrite(randomised_wing_dataset[RandRep %chin% reps_nums],
-         file=paste0("R:/Multivariate Dataset Sampler 2024/mr_wings_6traits_rando_", eight_sets[i,1],"_",eight_sets[i,2], "_datasets.csv"),
+         file=paste0("R:\\Multivariate Dataset Sampler 2024\\mr_wings_6traits_rando_", eight_sets[i,1],"_",eight_sets[i,2], "_datasets.csv"),
          quote=FALSE)
-  sink(paste0("R:/Multivariate Dataset Sampler 2024/RND_data_M_",eight_sets[i,1],"_",eight_sets[i,2],".txt"))
+  sink(paste0("R:\\Multivariate Dataset Sampler 2024\\RND_data_M_",eight_sets[i,1],"_",eight_sets[i,2],".txt"))
   cat(
-    'proc import datafile="R:/Multivariate Dataset Sampler 2024/mr_wings_6traits_rando_',eight_sets[i,1],"_",eight_sets[i,2],'_datasets.csv"
+    'proc import datafile="R:\\Multivariate Dataset Sampler 2024\\mr_wings_6traits_rando_',eight_sets[i,1],"_",eight_sets[i,2],'_datasets.csv"
 out=RNDmrwings dbms=csv replace; 
 getnames=yes; 
 run;
@@ -199,13 +199,13 @@ quit;
 \n
 \n
 PROC EXPORT DATA= WORK.M
-OUTFILE= "R:/Multivariate Dataset Sampler 2024/Randomised data/covpars_',eight_sets[i,1],"_",eight_sets[i,2],'.csv"
+OUTFILE= "R:\\Multivariate Dataset Sampler 2024\\Randomised data\\covpars_',eight_sets[i,1],"_",eight_sets[i,2],'.csv"
 DBMS=CSV REPLACE;
 PUTNAMES=YES;											
 run;											
 quit;
 PROC EXPORT DATA= WORK.converge
-OUTFILE= "R:/Multivariate Dataset Sampler 2024/Randomised data/converge_',eight_sets[i,1],"_",eight_sets[i,2],'.csv"
+OUTFILE= "R:\\Multivariate Dataset Sampler 2024\\Randomised data\\converge_',eight_sets[i,1],"_",eight_sets[i,2],'.csv"
 DBMS=CSV REPLACE;
 PUTNAMES=YES;											
 run;											

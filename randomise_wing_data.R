a2800f9062612b45d0a9009fc9caed360360f0f4
Cara Conradsen <uqcconra@uq.edu.au>
2024-03-24 03:43:45 +1000

    Added loop to write SAS code

--- randomise_wing_data.R
+++ randomise_wing_data.R
@@ -147,8 +147,71 @@ randomised_wing_dataset<- foreach(x = 1:ncol(all_sets), .packages = c("data.tabl
 setorderv(randomised_wing_dataset,
           c("RandRep","Gen", "Treat", "Line","Vial", "Ind"))
 
-# Output csv
-fwrite(randomised_wing_dataset,
-       file=paste0(outdir, "/mr_wings_6traits_rando_", n_set, "_datasets.csv"),
-       quote=FALSE)
+#### Part 5. Write 8 lots of code to run in SAS ####
+eight_sets <- data.frame(minRep=seq(from=0, to=875, length=8)+1,
+                         maxRep=seq(from=125, to=1000, length=8))
+
+for (i in 1:8) {
+  reps_nums <- seq(eight_sets[i,1],eight_sets[i,2])
+  
+  # Output csv outdir, 
+  fwrite(randomised_wing_dataset[RandRep %chin% reps_nums],
+         file=paste0("R:/Multivariate Dataset Sampler 2024/mr_wings_6traits_rando_", eight_sets[i,1],"_",eight_sets[i,2], "_datasets.csv"),
+         quote=FALSE)
+  sink(paste0("R:/Multivariate Dataset Sampler 2024/RND_data_M_",eight_sets[i,1],"_",eight_sets[i,2],".txt"))
+  cat(
+    'proc import datafile="R:/Multivariate Dataset Sampler 2024/mr_wings_6traits_rando_',eight_sets[i,1],"_",eight_sets[i,2],'_datasets.csv"
+out=RNDmrwings dbms=csv replace; 
+getnames=yes; 
+run;
+\n
+\n
+proc sort data = RNDmrwings;
+by RandRep Trait;
+run;
+quit;
+\n
+\n
+proc standard data=RNDmrwings out=stdRND std=1 mean=0;
+by RandRep Trait;
+var Score;
+run;
+quit;
+\n
+\n
+proc sort data = stdRND;
+by RandRep Treat Gen;
+run;
+quit;
+\n
+\n
+ods output convergencestatus=converge covparms=M;
+proc mixed data=stdRND method=reml lognote scoring=5;
+where multiout not in (1);
+by RandRep Treat Gen;
+class Treat Line Animal Vial Trait;
+model Score = Trait;
+random Trait / subject= Line type=un;
+random Trait / subject= Vial(Treat Line) type=un;
+repeated / subject = Animal(Treat Line Vial) type=un;
+run;
+quit;
+\n
+\n
+PROC EXPORT DATA= WORK.M
+OUTFILE= "R:/Multivariate Dataset Sampler 2024/Randomised data/covpars_',eight_sets[i,1],"_",eight_sets[i,2],'.csv"
+DBMS=CSV REPLACE;
+PUTNAMES=YES;											
+run;											
+quit;
+PROC EXPORT DATA= WORK.converge
+OUTFILE= "R:/Multivariate Dataset Sampler 2024/Randomised data/converge_',eight_sets[i,1],"_",eight_sets[i,2],'.csv"
+DBMS=CSV REPLACE;
+PUTNAMES=YES;											
+run;											
+quit;', sep = "")
+  sink()
+
+}
+
 
